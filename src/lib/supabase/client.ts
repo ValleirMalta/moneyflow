9026604eeabf18ab976aaaeeaa27f20b456f7e38
Valleir Malta <31601403+ValleirMalta@users.noreply.github.com>
2026-04-08 20:59:05 -0300

    Client function changed.

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,11 +2,7 @@ import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
   );
-}
-
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_URL não definida. Configure o .env.local");
 }
